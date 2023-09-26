74e66d8ecd7841babb6cda650c7a21f77e15f8ad
tony <hireriversanthony@gmail.com>
2023-09-26 02:34:44 -0400

    working on hero section

---
+++ main.js
@@ -0,0 +1,8 @@
+// styling for the first hero input button 
+const clearInput = () => {
+    const input = document.getElementsByTagName("input")[0];
+    input.value = "";
+}
+
+const clearBtn = document.getElementById("clear-btn");
+clearBtn.addEventListener("click", clearInput);
