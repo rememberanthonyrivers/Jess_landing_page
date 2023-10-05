c49c3c1f695f18c3878337a41916147b18ce6cce
tony <hireriversanthony@gmail.com>
2023-10-05 11:59:40 -0400

    switching the design layout

--- main.js
+++
@@ -1,7 +0,0 @@
-// styling for the first hero input button 
-const clearInput = () => {
-    const input = document.getElementsByTagName("input")[0];
-    input.value = "";
-}
-const clearBtn = document.getElementById("clear-btn");
-clearBtn.addEventListener("click", clearInput);
